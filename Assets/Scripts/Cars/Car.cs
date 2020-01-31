926825fedae61314dfdc5f30883d65dba5c5ef86
Michael Shalom <michaelpentax@gmail.com>
2020-01-31 11:17:52 +0200

    car class

---
+++ Assets/Scripts/Cars/Car.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using carcikir
+
+public class Car
+{
+    // public
+    public enum CarColor
+    {
+        blue,
+        purple
+    }
+    public enum CarType
+    {
+        sedan,
+        bus,
+        coupe
+    }
+    public CarColor Color;
+    public ushort seats;
+    public float desiredSpeed;
+    public float actualSpeed;
+    public carType type;
+
+    
+
+}
