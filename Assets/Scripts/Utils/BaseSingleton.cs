b8044cdbdfac6c4468c9756b47b318b26fcb7640
Doron Nacash <doronn@jellybtn.com>
2020-01-31 10:21:26 +0200

    Plugins

---
+++ Assets/Scripts/Utils/BaseSingleton.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public abstract class BaseSingleton<T> : MonoBehaviour where T : BaseSingleton<T>
+    {
+        [SerializeField]
+        private bool _isPersistent;
+        
+        private static T _instance;
+
+        public static T GetInstance(bool? isPersistent = null)
+        {
+            if (_instance)
+            {
+                if (isPersistent.HasValue)
+                {
+                    _instance._isPersistent = isPersistent.Value && _instance._isPersistent;
+                }
+
+                return _instance;
+            }
+
+            var instance = FindObjectOfType<T>();
+
+            var isSetToPersistent = isPersistent ?? false;
+            if (instance)
+            {
+                if (instance._isPersistent || isSetToPersistent)
+                {
+                    instance._isPersistent = true;
+                    DontDestroyOnLoad(instance);
+                }
+
+                _instance = instance;
+                return _instance;
+            }
+
+            var gameObject = new GameObject();
+            instance = gameObject.AddComponent<T>();
+            instance._isPersistent = isSetToPersistent;
+
+            _instance = instance;
+            return _instance;
+        }
+
+        protected void Awake()
+        {
+            if (_isPersistent)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        protected void OnDestroy()
+        {
+            _instance = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/EventManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace Utils
+{
+    public class EventManager : BaseSingleton<EventManager>
+    {
+        public Dictionary<GGJEventType, UnityEvent> events;
+        
+        public void Subscribe(GGJEventType eventType, UnityAction onEvent)
+        {
+            if (!events.TryGetValue(eventType, out var unityEvent))
+            {
+                unityEvent = new UnityEvent();
+                events.Add(eventType, unityEvent);
+            }
+
+            unityEvent.AddListener(onEvent);
+        }
+
+        public void Publish(GGJEventType eventType)
+        {
+            if (!events.TryGetValue(eventType, out var unityEvent))
+            {
+                return;
+            }
+            
+            unityEvent.Invoke();
+        }
+
+        public void UnSubscribe(GGJEventType eventType, UnityAction eventToRemove)
+        {
+            if (!events.TryGetValue(eventType, out var unityEvent))
+            {
+                return;
+            }
+            
+            unityEvent.RemoveListener(eventToRemove);
+        }
+    }
+}
