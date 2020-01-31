6d26a0d17a6b7d56590fd4326cddd127a92a14c0
Idan Nachman <idann@jellybtn.com>
2020-01-31 12:29:53 +0200

    Added game economy

---
+++ Assets/Scripts/GameConfigs/Economy.cs
@@ -0,0 +1,58 @@
+using System;
+using RotaryHeart.Lib.SerializableDictionary;
+using UnityEngine;
+
+namespace UnityTemplateProjects.GameConfigs
+{
+    [CreateAssetMenu(fileName = "Game Economy", menuName = "Traffix/Game Economy")]
+    public class Economy : ScriptableObject
+    {
+        [SerializeField] private LevelEconomy[] levelEconomies;
+    }
+    
+    [Serializable]
+    public class LevelEconomy
+    {
+        [SerializeField] public CarTypeDefinitions carTypeDefinitions;
+        [SerializeField] public IntWeightedRandomDefinition[] initialSeatsTaken;
+        [SerializeField] public ColorWeightedRandomDefinition[] carColors;
+        [SerializeField] public CarTypeWeightedRandomDefinition[] carTypes;
+        [SerializeField] public MinMaxDefinition carSpeed;
+        [Min(0)][SerializeField] public int targetScore;
+        [Min(0)][SerializeField] public int duration;
+    }
+    
+    [Serializable]
+    public class CarTypeDefinitions : SerializableDictionaryBase<Car.CarType, int>
+    {
+    }
+    
+    [Serializable]
+    public class WeightedRandomDefinition<T>
+    {
+        [Min(0)][SerializeField] public int weight;
+        [SerializeField] public T item;
+    }
+    
+    [Serializable]
+    public class IntWeightedRandomDefinition : WeightedRandomDefinition<int>
+    {
+    }
+    
+    [Serializable]
+    public class ColorWeightedRandomDefinition : WeightedRandomDefinition<Color>
+    {
+    }
+    
+    [Serializable]
+    public class CarTypeWeightedRandomDefinition : WeightedRandomDefinition<Car.CarType>
+    {
+    }
+    
+    [Serializable]
+    public class MinMaxDefinition
+    {
+        [SerializeField] public float minValue;
+        [SerializeField] public float maxValue;
+    }
+}
