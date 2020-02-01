bf2416af7212e69742547620ba7d94d8de449eb5
Doron Nacash <doronn@jellybtn.com>
2020-02-01 15:05:40 +0200

    Start level ui

---
+++ Assets/Scripts/UiControllers/PlayLevelButtonEventDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace UnityTemplateProjects.UiControllers
+{
+    public class PlayLevelButtonEventDispatcher : MonoBehaviour
+    {
+        public void OnPlayLevelClicked()
+        {
+            
+        }
+    }
+}
