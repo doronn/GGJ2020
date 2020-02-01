f9cc5209a30d536b7049cf08092e8a8c22961354
Idan Nachman <idann@jellybtn.com>
2020-02-01 16:35:47 +0200

    Economy formula

--- Assets/Scripts/Level/LevelEconomyProvider.cs
+++ Assets/Scripts/Level/LevelEconomyProvider.cs
@@ -1,6 +1,8 @@
+using System;
 using UnityEngine;
 using UnityTemplateProjects.GameConfigs;
 using Utils;
+using Object = UnityEngine.Object;
 
 namespace UnityTemplateProjects.Level
 {
@@ -8,31 +10,27 @@ namespace UnityTemplateProjects.Level
     {
         public static LevelEconomy GetEconomyForLevel(int level)
         {
-            // TODO: convert to a formula
             var levelEconomy = new LevelEconomy
             {
                 initialSeatsTaken = new WeightedRandomProvider<ushort>(),
                 carColors = new WeightedRandomProvider<Color>(),
                 carTypes = new WeightedRandomProvider<CarType>(),
-                carSpeed = new MinMaxDefinition(4, 6),
+                carSpeed = new MinMaxDefinition(3 + Math.Min((int)Math.Ceiling(level/100f), 2), 5 + Math.Min((int)Math.Ceiling(level/100f), 2)),
                 pedestrianGenerationInterval = new MinMaxDefinition(10, 20),
                 people = new WeightedRandomProvider<Object>(),
-                targetScore = 100,
-                duration = 30,
+                targetScore = 20 + Math.Min(level / 5, 30),
+                duration = 40 - Math.Min(level / 2, 20),
                 lanes = 3,
-                carGenerationInterval = 3f
+                carGenerationInterval = 2
             };
             
-            levelEconomy.initialSeatsTaken.Add(1, 3);
-            levelEconomy.carColors.Add(new Color(0.09765625F, 0.2851563F, 0.5195313F), 1);
-            levelEconomy.carColors.Add(new Color(0.1679688F, 0.4765625F, 0.7109375F), 1);
-            levelEconomy.carColors.Add(new Color(0.3828125F, 0.1679688F, 0.4804688F), 1);
-            levelEconomy.carColors.Add(new Color(0.9179688F, 0.4921875F, 0.578125F), 1);
-            levelEconomy.carColors.Add(new Color(0.90625F, 0.3789063F, 0.4765625F), 1);
-            levelEconomy.carColors.Add(new Color(0.8476563F, 0.3671875F, 0.6289063F), 1);
-            levelEconomy.carColors.Add(new Color(0.8164063F, 0.9179688F, 0.953125F), 1);
-            levelEconomy.carColors.Add(new Color(0.359375F, 0.765625F, 0.8242188F), 1);
-            levelEconomy.carTypes.Add(CarType.Sedan, 10);
+            levelEconomy.initialSeatsTaken.Add(1, 20);
+            levelEconomy.initialSeatsTaken.Add(2, 1);
+            levelEconomy.carColors.Add(new Color(0.3828125F, 0.1679688F, 0.4804688F), 20);
+            levelEconomy.carColors.Add(new Color(0.9179688F, 0.4921875F, 0.578125F), 20);
+            levelEconomy.carColors.Add(new Color(0.8476563F, 0.3671875F, 0.6289063F), Math.Min(5 + level / 5, 10));
+            levelEconomy.carColors.Add(new Color(0.359375F, 0.765625F, 0.8242188F), Math.Min(5 + level / 5, 10));
+            levelEconomy.carTypes.Add(CarType.Sedan, 20);
             levelEconomy.carTypes.Add(CarType.Coupe, 3);
             levelEconomy.carTypes.Add(CarType.Bus, 1);
             
